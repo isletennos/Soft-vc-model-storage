c51fd09bfdff9fa10ec3e562af82ae4604f531f4
isletennos <isle.tennos@gmail.com>
2022-08-13 17:55:17 +0900

    URL変更

--- acoustic/model.py
+++ acoustic/model.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 from torch.nn.modules.utils import consume_prefix_in_state_dict_if_present
 
 URLS = {
-    "hubert-discrete": "https://github.com/bshall/acoustic-model/releases/download/v0.1/hubert-discrete-d49e1c77.pt",
-    "hubert-soft": "https://github.com/bshall/acoustic-model/releases/download/v0.1/hubert-soft-0321fd7e.pt",
+    "hubert-discrete": "https://github.com/isletennos/Soft-vc-model-storage/blob/main/models/model-best.pt",
+    "hubert-soft": "https://github.com/isletennos/Soft-vc-model-storage/blob/main/models/model-best.pt",
 }
 
 
